053fa1a22e7f7505d940902c66ba9d9c0826405a
csiszaralex <csiszaralex@gmail.com>
2023-04-10 19:55:28 +0200

    :card_file_box: Edit models for prepare mongoose

--- models/kocsi.js
+++ models/kocsi.js
@@ -1,10 +1,67 @@
-const Kocsi = {
-  Sorszam: Number,
-  Szam: Number,
-  Ulohelyek: Number,
-  Helyjegy: Boolean,
-  //TODO vonat majd Vonat típus lesz
-  vonat: String
+// const Kocsi2 = {
+//   Sorszam: Number,
+//   Szam: Number,
+//   Ulohelyek: Number,
+//   Helyjegy: Boolean,
+//   //TODO vonat majd Vonat típus lesz
+//   vonat: String,
+// };
+
+// module.exports = Kocsi;
+
+/**
+ * Kocsi model (mock)
+ * @constructor
+ */
+const Kocsi = function () {};
+
+const KocsiInstanceMock = {
+  id: 'kocsi1',
+  Sorszam: 1,
+  Szam: 1,
+  Ulohelyek: 100,
+  Helyjegy: true,
+  vonat: 'vonat1',
+};
+
+/**
+ * Find one element with criteria
+ * @param criteria
+ * @param  cb
+ * @returns {*}
+ */
+Kocsi.findOne = function (criteria, cb) {
+  return cb(null, KocsiInstanceMock);
+};
+
+/**
+ * Find all elements with criteria
+ * @param  criteria
+ * @param  cb
+ * @returns {*}
+ */
+Kocsi.find = function (criteria, cb) {
+  return cb(null, [KocsiInstanceMock, KocsiInstanceMock, KocsiInstanceMock]);
+};
+
+/**
+ * Save the item to DB
+ * @param cb
+ * @returns {*}
+ */
+Kocsi.prototype.save = function (cb) {
+  return cb(null, this);
+}
+
+/**
+ * Delete an object
+ * @param cb
+ * @returns {*}
+ */
+Kocsi.prototype.remove = function (cb) {
+  return cb(null);
 }
 
 module.exports = Kocsi;
+
+
--- models/vonat.js
+++ models/vonat.js
@@ -1,7 +1,55 @@
-const Vonat = {
-  Szam: Number,
-  Uzemanyag: String,
+// const Vonat = {
+//   Szam: Number,
+//   Uzemanyag: String,
 
+// };
+
+// module.exports = Vonat;
+
+const Vonat = function () {};
+
+const VonatInstanceMock = {
+  id: 'vonat1',
+  Szam: 1,
+  Uzemanyag: 'diesel',
+};
+
+/**
+ * Find one element with criteria
+ * @param  criteria
+ * @param  cb
+ * @returns {*}
+ */
+Vonat.findOne = function (criteria, cb) {
+  return cb(null, VonatInstanceMock);
+};
+
+/**
+ * Find all elements with criteria
+ * @param  criteria
+ * @param  cb
+ * @returns {*}
+ */
+Vonat.find = function (criteria, cb) {
+  return cb(null, [VonatInstanceMock, VonatInstanceMock, VonatInstanceMock]);
+};
+
+/**
+ * Save the item to DB
+ * @param cb
+ * @returns {*}
+ */
+Vonat.prototype.save = function (cb) {
+  return cb(null, this);
+};
+
+/**
+ * Delete an object
+ * @param cb
+ * @returns {*}
+ */
+Vonat.prototype.remove = function (cb) {
+  return cb(null);
 };
 
 module.exports = Vonat;

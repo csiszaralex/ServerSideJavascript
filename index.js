d2fd898c030a19ecd8201e7804cf6c0ba05905f3
csiszaralex <csiszaralex@gmail.com>
2023-04-10 18:22:46 +0200

    :sparkles: Update main index from module to commonjs

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-//TODO make the route to static
-app.use('/', express.static('static'));
+app.use(express.static('static'));
+
+//TODO ejs
+// app.use(bodyParser.urlencoded());
+// app.use(bodyParser.json());
+
+require('./routes/index')(app);
+
+// Error Handling
+// app.use((err, req, res, next) => {
+//   res.end('Problem...');
+//   console.log(err);
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');

ce0e7abf3962e6e038d1e8021f82eedc1bb31b9b
csiszaralex <csiszaralex@gmail.com>
2023-04-13 23:10:22 +0200

    :rewind: Rewind models to good

--- models/vonat.js
+++ models/vonat.js
@@ -1,55 +1,6 @@
-// const Vonat = {
-//   Szam: Number,
-//   Uzemanyag: String,
-
-// };
-
-// module.exports = Vonat;
-
-const Vonat = function () {};
-
-const VonatInstanceMock = {
-  id: 'vonat1',
-  Szam: 1,
-  Uzemanyag: 'diesel',
-};
-
-/**
- * Find one element with criteria
- * @param  criteria
- * @param  cb
- * @returns {*}
- */
-Vonat.findOne = function (criteria, cb) {
-  return cb(null, VonatInstanceMock);
-};
-
-/**
- * Find all elements with criteria
- * @param  criteria
- * @param  cb
- * @returns {*}
- */
-Vonat.find = function (criteria, cb) {
-  return cb(null, [VonatInstanceMock, VonatInstanceMock, VonatInstanceMock]);
-};
-
-/**
- * Save the item to DB
- * @param cb
- * @returns {*}
- */
-Vonat.prototype.save = function (cb) {
-  return cb(null, this);
-};
-
-/**
- * Delete an object
- * @param cb
- * @returns {*}
- */
-Vonat.prototype.remove = function (cb) {
-  return cb(null);
+const Vonat = {
+  Szam: Number,
+  Uzemanyag: String,
 };
 
 module.exports = Vonat;

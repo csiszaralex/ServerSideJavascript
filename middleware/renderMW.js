4e09f63cf0ab38f87c6bbe29349a927f57491559
csiszaralex <csiszaralex@gmail.com>
2023-04-14 03:35:09 +0200

    :sparkles: Adds res.locals for render middleware

--- middleware/renderMW.js
+++ middleware/renderMW.js
@@ -2,9 +2,7 @@
  * Using the template engine render the values into the template
  */
 module.exports = function (objectrepository, viewName) {
-
   return function (req, res) {
-    res.render(viewName);
+    res.render(viewName, res.locals);
   };
-
 };

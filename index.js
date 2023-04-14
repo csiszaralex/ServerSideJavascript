dfa8107b4cfe624b9465d9d7cddfd2470b3eda1d
csiszaralex <csiszaralex@gmail.com>
2023-04-14 03:30:20 +0200

    :goal_net: Adds error handling

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.locals.error = {};
+  next();
+});
 
 require('./routes/index')(app);
 
-// Error Handling
-// app.use((err, req, res, next) => {
-//   res.end('Problem...');
-//   console.log(err);
-// });
+app.use((err, req, res, next) => {
+  res.end('Problem...');
+  console.log(err);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
